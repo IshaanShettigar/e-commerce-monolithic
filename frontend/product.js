7096d5c131b4180c56750df54f5e007c45f3fe8b
Ishaan Shettigar <ishaanshettigar@gmail.com>
2023-07-15 10:12:35 +0530

    slight

--- frontend/product.js
+++ frontend/product.js
@@ -9,3 +9,25 @@ const p_details = document.getElementById("p-details");
 img.src = `./images/plants/${productId}.png`
 p_name.innerHTML = `${productId}`
 
+
+const addToCartBtn = document.getElementById('add-to-cart')
+const prodQty = document.getElementById('prod-qty')
+addToCartBtn.addEventListener('click', (evt) => {
+    console.log("Add to cart");
+    // localStorage.setItem('cart',)
+    const item = localStorage.getItem('cart')
+    console.log(JSON.parse(item));
+    if (item) {
+        if (item[productId]) {
+            // item[productId] = 
+
+        }
+    }
+    else {
+        let cartJSON = {
+            "name": productId,
+            "quantity": prodQty.value
+        }
+        localStorage.setItem('cart', JSON.stringify(cartJSON))
+    }
+})
