6491c167adfb310b8a3a09b4b4cebe40b033c201
Hari Ganesan <hariganation@gmail.com>
2023-08-09 18:59:04 +0530

    Fixed add-to-cart in product page

--- frontend/product.js
+++ frontend/product.js
@@ -3,31 +3,37 @@ const productId = urlParams.get('product');
 
 const img = document.getElementById("MainImg")
 const p_name = document.getElementById("p-name");
-const p_price = document.getElementById("p-price");
 const p_details = document.getElementById("p-details");
+const addToCartBtn = document.getElementById('add-to-cart')
+const prodQty = document.getElementById('prod-qty')
+const price = document.getElementById('p-price').innerText.replace('₹ ', '')
 
 img.src = `./images/plants/${productId}.png`
 p_name.innerHTML = `${productId}`
 
 
-const addToCartBtn = document.getElementById('add-to-cart')
-const prodQty = document.getElementById('prod-qty')
-addToCartBtn.addEventListener('click', (evt) => {
-    console.log("Add to cart");
-    // localStorage.setItem('cart',)
-    const item = localStorage.getItem('cart')
-    console.log(JSON.parse(item));
-    if (item) {
-        if (item[productId]) {
-            // item[productId] = 
 
-        }
+addToCartBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    let cart = localStorage.getItem('cart');
+    cart = cart ? JSON.parse(cart): [];
+    console.log(cart)
+
+    // if (!Array.isArray(cart)) {
+    //     console.error('Unexpected value in cart:', cart);
+    //     cart = [];
+    // }
+    const existingProductIndex = cart.findIndex(item => item.name === productId);
+
+    if (existingProductIndex > -1) {
+        cart[existingProductIndex].quantity += parseInt(prodQty.value);
+    } else {
+        cart.push({
+            name: productId,
+            quantity: parseInt(prodQty.value),
+            price: price
+        });
     }
-    else {
-        let cartJSON = {
-            "name": productId,
-            "quantity": prodQty.value
-        }
-        localStorage.setItem('cart', JSON.stringify(cartJSON))
-    }
-})
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+});
